45dc0c0908cf78757c16fd817e90117777cbf47c
PradeepArivazhagan <pradeeparivazhagan123@gmail.com>
2025-01-31 10:18:49 +0530

    fix: padding in wishlish and cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -49,7 +49,7 @@ const Cart = () => {
     <div className="min-h-lvh bg-slate-100 pt-19 pb-10 md:pt-20 lg:px-28">
       <div className="my-2 min-h-lvh bg-white rounded-xs p-4">
         <h1 className="text-2xl font-semibold">Your Cart</h1>
-        <div className="p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="py-4 md:p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {productList.map((product) => (
             <CartProduct key={product.id} productDetails={product} />
           ))}
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -35,7 +35,7 @@ const Wishlist = () => {
     <div className="min-h-lvh bg-slate-100 pt-19 pb-10 md:pt-20 lg:px-28">
       <div className="my-2 min-h-lvh bg-white rounded-xs p-4">
         <h1 className="text-2xl font-semibold">Your Wishlist</h1>
-        <div className="p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="py-4 md:p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {wishlistProducts.map((product) => (
             <WishlistProduct key={product.id} productDetails={product} />
           ))}
